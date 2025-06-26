2aa665771ca53b06663d9fce192e944672287175
sofisantamaria <sofia256sp@gmail.com>
2025-06-26 16:12:51 -0600

    EjercicioPractico1

--- CasoPractico1/src/test/java/com/farmacia/farmacia/FarmaciaApplicationTests.java
+++ CasoPractico1/src/test/java/com/farmacia/farmacia/FarmaciaApplicationTests.java
@@ -6,8 +6,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class FarmaciaApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
-
+    @Test
+    void contextLoads() {
+    }
 }
